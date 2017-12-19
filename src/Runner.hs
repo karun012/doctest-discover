df327799f7d74bcfdcb791323c0bca5c2c398c54
patrick brisbin <pbrisbin@gmail.com>
2017-12-19 11:06:49 -0500

    Don't prepend / to included directories
    Current behavior of adding "-i/src" would include "/src", not "./src",
    as was likely intended.
    
    Fixes #11.

--- src/Runner.hs
+++ src/Runner.hs
@@ -8,7 +8,7 @@ import System.FilePath.Posix
 
 -- | Generates doctest driver
 --
--- >>> let expected = unlines ["module Main where", "import Test.DocTest", "main :: IO ()", "main = doctest [\"-i/src\",\"foo.hs\",\"bar.hs\"]"]
+-- >>> let expected = unlines ["module Main where", "import Test.DocTest", "main :: IO ()", "main = doctest [\"-isrc\",\"foo.hs\",\"bar.hs\"]"]
 -- >>> let actual = driver ["foo.hs", "bar.hs", "baz.qux"] Nothing
 -- >>> expected == actual
 -- True
@@ -19,25 +19,25 @@ driver files config = unlines $ ["module Main where", "import Test.DocTest", "ma
 -- | Generates doctest configuration
 --
 -- >>> generateConfig ["foo.hs", "bar.hs", "baz.qux"] Nothing
--- ["-i/src","foo.hs","bar.hs"]
+-- ["-isrc","foo.hs","bar.hs"]
 --
 -- >>> let config = Just (Config (Just ["bar.hs"]) Nothing)
 -- >>> generateConfig ["foo.hs", "bar.hs", "baz.qux"] config
--- ["-i/src","foo.hs"]
+-- ["-isrc","foo.hs"]
 --
 -- >>> let config = Just (Config Nothing (Just ["qux"]))
 -- >>> generateConfig ["foo.hs", "bar.hs", "baz.qux"] config
--- ["-i/qux","foo.hs","bar.hs"]
+-- ["-iqux","foo.hs","bar.hs"]
 --
 -- >>> let config = Just (Config (Just ["bar.hs"]) (Just ["qux"]))
 -- >>> generateConfig ["foo.hs", "bar.hs", "baz.qux"] config
--- ["-i/qux","foo.hs"]
+-- ["-iqux","foo.hs"]
 --
 generateConfig :: [FilePath] -> Maybe Config -> [String]
-generateConfig files (Just (Config Nothing (Just sourceFolders))) = ((++) (map ("-i/"++) sourceFolders) . notCurrentAndParent . filterHaskellSources) files
-generateConfig files (Just (Config (Just ignoreList) Nothing)) = ((:) "-i/src" . filter (`notElem` ignoreList) . notCurrentAndParent . filterHaskellSources) files
-generateConfig files (Just (Config (Just ignoreList) (Just sourceFolders))) = ((++) (map ("-i/"++) sourceFolders) . filter (`notElem` ignoreList) . notCurrentAndParent . filterHaskellSources) files
-generateConfig files _ = ((:) "-i/src" . notCurrentAndParent . filterHaskellSources) files
+generateConfig files (Just (Config Nothing (Just sourceFolders))) = ((++) (map ("-i"++) sourceFolders) . notCurrentAndParent . filterHaskellSources) files
+generateConfig files (Just (Config (Just ignoreList) Nothing)) = ((:) "-isrc" . filter (`notElem` ignoreList) . notCurrentAndParent . filterHaskellSources) files
+generateConfig files (Just (Config (Just ignoreList) (Just sourceFolders))) = ((++) (map ("-i"++) sourceFolders) . filter (`notElem` ignoreList) . notCurrentAndParent . filterHaskellSources) files
+generateConfig files _ = ((:) "-isrc" . notCurrentAndParent . filterHaskellSources) files
 
 -- | Filters out current and parent directories 
 --
